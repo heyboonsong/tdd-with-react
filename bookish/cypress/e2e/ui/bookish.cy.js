d906d493594ffcafef89a4102342bbb3132d13d1
song <heyboonsong@gmail.com>
2022-07-29 14:43:06 +0700

    set up teardown

--- bookish/cypress/e2e/ui/bookish.cy.js
+++ bookish/cypress/e2e/ui/bookish.cy.js
@@ -1,4 +1,35 @@
+import axios from "axios";
+
 describe("Bookish application", function () {
+  before(() => {
+    return axios
+      .delete("http://localhost:8080/books?_cleanup=true")
+      .catch((err) => err);
+  });
+
+  afterEach(() => {
+    return axios
+      .delete("http://localhost:8080/books?_cleanup=true")
+      .catch((err) => err);
+  });
+
+  beforeEach(() => {
+    const books = [
+      { name: "Refactoring", id: 1 },
+      { name: "Domain-driven desing", id: 2 },
+      { name: "Builing Microservices", id: 3 },
+    ];
+    return Promise.all(
+      books.map((item) =>
+        axios
+          .post("http://localhost:8080/books", item, {
+            headers: { "Conent-Type": "application/json" },
+          })
+          .catch((err) => err)
+      )
+    );
+  });
+
   it("Visits the bookish", function () {
     cy.visit("http://localhost:3000/");
     cy.get('h2[data-test="heading"]').contains("Bookish");
@@ -8,9 +39,13 @@ describe("Bookish application", function () {
     cy.visit("http://localhost:3000/");
     cy.get('div[data-test="book-list"]').should("exist");
     cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(2);
+      expect(books).to.have.length(3);
       const titles = [...books].map((q) => q.querySelector("h2").innerHTML);
-      expect(titles).to.deep.equal(["Refactoring", "Domain-driven desing"]);
+      expect(titles).to.deep.equal([
+        "Refactoring",
+        "Domain-driven desing",
+        "Builing Microservices",
+      ]);
     });
   });
 });
--- bookish/src/App.js
+++ bookish/src/App.js
@@ -1,8 +1,16 @@
 import { Typography } from "@material-ui/core";
 import BookList from "./BookList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
-  const books = [{ name: "Refactoring" }, { name: "Domain-driven desing" }];
-
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get("http://localhost:8080/books");
+      setBooks(res.data);
+    };
+    fetchBooks();
+  }, []);
   return (
     <div className="App">
       <Typography variant="h2" component="h2" data-test="heading">
