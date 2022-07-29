6613b364702ab96cdabe35b71087e1ef4a92ff75
song <heyboonsong@gmail.com>
2022-07-29 13:31:33 +0700

    add book list

--- bookish/src/App.js
+++ bookish/src/App.js
@@ -1,11 +1,14 @@
 import { Typography } from "@material-ui/core";
-
+import BookList from "./BookList";
 function App() {
+  const books = [{ name: "Refactoring" }, { name: "Domain-driven desing" }];
+
   return (
     <div className="App">
       <Typography variant="h2" component="h2" data-test="heading">
         Bookish
       </Typography>
+      <BookList books={books} />
     </div>
   );
 }
